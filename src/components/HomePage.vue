bb9d022f4bc277a5d944fbe24971e6d1c8a4df3d
Nikhil George <nikhgeo@gmail.com>
2021-04-30 15:50:41 +0530

    Initial Commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NavBar />
+    <ContentCard />
+    <AboutMeCard />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NavBar from "./NavBar";
+import ContentCard from "@/components/ContentCard.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  components: {
+    NavBar,
+    ContentCard,
+    Footer,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
